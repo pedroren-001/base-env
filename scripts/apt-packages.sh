3ce989be8b495122f1773e3ae35c56eacdb8b2d3
pedroren <renyanjie01@qq.com>
2025-11-23 19:58:43 +0800

    add apt package

--- scripts/apt-packages.sh
+++ scripts/apt-packages.sh
@@ -1,4 +1,4 @@
-tools=("make" "cmake" "subversion" "git")
+tools=("make" "cmake" "subversion" "git" "fzf" "tmux" "universal-ctags")
 for tool in "${tools[@]}"; do
     echo "installing $tool"
     sudo apt install -y $tool
